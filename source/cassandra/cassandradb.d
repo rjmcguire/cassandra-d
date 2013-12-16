3597bc1b49e1875f187ee1cc3913d7ab39db170c
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-12-16 18:56:28 +0100

    Add port-less overload of `connectMongoDB`.

--- source/cassandra/cassandradb.d
+++ source/cassandra/cassandradb.d
@@ -5,3 +5,7 @@ import cassandra.client;
 CassandraClient connectCassandraDB(string host, ushort port) {
 	return new CassandraClient(host, port);
 }
+
+CassandraClient connectCassandraDB(string host) {
+	return new CassandraClient(host);
+}
