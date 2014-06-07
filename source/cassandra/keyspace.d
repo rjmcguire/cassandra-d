09f1bf783829beafac63c3adcc838f39642e2af4
Sönke Ludwig <sludwig@rejectedsoftware.com>
2014-06-07 19:09:07 +0200

    Add a first version of CassandraKeyspace.createTable.

--- source/cassandra/keyspace.d
+++ source/cassandra/keyspace.d
@@ -30,7 +30,31 @@ struct CassandraKeyspace {
 
 	CassandraTable getTable(string table) { return CassandraTable(this, table); }
 
-	CassandraResult query(string q, Consistency consistency = Consistency.any)
+	CassandraTable createTable(TABLE, ATTRIBUTES...)(string name, PrimaryKeyAttribute primary_key, ATTRIBUTES attributes)
+	{
+		//assert(isIdentifier(name));
+		auto stmt = appender!string();
+		stmt ~= "CREATE TABLE "~name~" (";
+		foreach (i, T; typeof(TABLE.tupleof)) {
+			stmt ~= "\n  " ~ __traits(identifier, TABLE.tupleof[i]) ~ " ";
+			stmt ~= bestCassandraType!T ~ ",";
+		}
+
+		stmt ~= "\n  PRIMARY KEY (";
+		if (primary_key.partitionKey.length > 1) {
+			stmt ~= "(" ~ primary_key.partitionKey.join(", ") ~ ")";
+			if (primary_key.clusteringColumns.length)
+				stmt ~= ", " ~ primary_key.clusteringColumns.join(", ");
+		} else stmt ~= (primary_key.partitionKey ~ primary_key.clusteringColumns).join(", ");
+		stmt ~= ")\n)";
+
+		// TODO: support WITH attributes!
+
+		query(stmt.data);
+		return CassandraTable(this, name);
+	}
+
+	CassandraResult query(string q, Consistency consistency = Consistency.one)
 	{
 		auto conn = m_client.lockConnection();
 		conn.useKeyspace(m_name);
@@ -50,4 +74,72 @@ struct CassandraKeyspace {
 		// TODO: assert(stmt.keyspace is this);
 		return conn.execute(conn, stmt, args);
 	}
+}
+
+
+/+/**
+	Attribute for declaring the primary key of a table.
+
+	Params:
+		column_names = The names of the columns that make up the primary key.
+			This can be one or more columns, of which the first one will be
+			the partition key.
+		partition_key = An array of column names used as a composite
+			partition key.
+		clustering_columns = Additional columns to include for the primary key
+			to determine the clustering order.
+*/
+PrimaryKeyAttribute primaryKey(string[] column_names...)
+{
+	assert(column_names.length >= 1);
+	return PrimaryKeyAttribute([column_names[0]], column_names[1 .. $]);
+}
+/// ditto
+PrimaryKeyAttribute primaryKey(string[] partition_key, string[] clustering_columns...)
+{
+	assert(partition_key.length >= 1);
+	return PrimaryKeyAttribute(partition_key, clustering_columns);
+}
+
+/** Examples translated from the CQL documentation
+
+	See also $(LINK http://www.datastax.com/documentation/cql/3.0/cql/cql_reference/create_table_r.html).
+*/
+unittest {
+	import std.uuid;
+
+	// single primary key: user_name
+	@primaryKey("user_name")
+	struct users {
+		string user_name;
+		string password;
+		string gender;
+		string session_token;
+		string state;
+		long birth_year;
+	}
+
+	// compound primary key: empID and deptID
+	@primaryKey("empID", "deptID")
+	struct emp {
+		int empID;
+		int deptID;
+		string first_name;
+		string last_name;
+	}
+
+	// composite partition key: block_id and breed
+	// additional clustering columne: color and short_hair
+	@primaryKey(["block_id", "breed"], "color", "short_hair")
+	struct Cats {
+		UUID block_id;
+		string breed;
+		string color;
+		bool short_hair;
+	}
+}+/
+
+struct PrimaryKeyAttribute {
+	string[] partitionKey;
+	string[] clusteringColumns;
 }
